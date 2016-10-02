5950dcb03fdfb8594cabb164fcb2a9dfc94d978a
vagrant <vagrant@homestead>
2016-10-02 16:39:38 +0000

    Pobierz i wyswietl dane na temat wybranego kraju

--- app/Http/Controllers/KrajController.php
+++ app/Http/Controllers/KrajController.php
@@ -20,7 +20,12 @@ class KrajController extends Controller
     	return view('Kraj')->with('kraj', $kraj);
     }
 
-
+    /** 
+    *funkcja zwraca Dane Kraju
+    * @param string - nazwa kraju
+    * @return - array($nazwa_kraju => $daneKraju), gdzie $daneKraju to tez array 
+    *
+    */
     private function zwrocDaneKraju($nazwa_kraju) {
 
     	//sluzy do testowania wyswietlania kraju
@@ -39,16 +44,65 @@ class KrajController extends Controller
 			return $kraj; 		
     	}
 
-    	$krajBaza = \App\factbook_countries::find(3);
+    	//odczytaj dane na temat danego kraju
+    	$countryid = $this->znajdzIdKraju( $nazwa_kraju );
+    	$daneKrajuJSON = $this->daneKrajuJSON( $countryid );
 
+    	//utworz zmienna zawierajaca dane ktora zostanie zwrocona
+    	$daneKraju = array();
+
+    	//zmien pobrane dane kraju z bazy danych na zmienna gotowa do wyswietlania
+    	//iteracja po kazdym elemencie zmiennej JSON zawierajacym dane kraju 
+    	//odczytaj nazwe pola 
+    	//zapisza w nowymm array $daneKraju nazwe pola i jego zawartosc
+    	foreach ( $daneKrajuJSON as $dane ) {
+     		$fieldid = $dane->fieldid;
+    		$nazwaPola = $this->zwrocNazwePola( $fieldid );  
+    		$value = $dane->value;
+ 
+    		$daneKraju[ $nazwaPola ] = $value;
+    	}
+
+
+    	//utworz zwracana zmienna
       	$kraj = array(
-   			$nazwa_kraju => array(
-   				"wstep" =>$krajBaza->name,
-   				"geografia" => "Bez dostepu do morza, kraj polozony pomiedzy dwoma wrogimi krajami",
-   				"gdp" => 123456)
-
+   			$nazwa_kraju => $daneKraju
 		);
 		
 		return $kraj; 	
     }
+
+    /**
+    * Znajdz id danego kraju podajac jego nazwe
+    * @param - string - nazwa kraju - 
+	* @return - number - countryid - Id Kraju
+    */
+    private function znajdzIdKraju($nazwa_kraju) {
+    	$krajBaza = \App\factbook_countries::where('name', $nazwa_kraju)->get();
+    	$countryid = $krajBaza[0]->id;
+    	return $countryid;
+    }
+
+
+    /**
+    *Funkcja zwraca dane/informacje pobrane z bazy danychna temat danego kraju
+    * @param - int countryid
+    * @return - JSON zawierajacy dane z bazy
+    */
+    private function daneKrajuJSON($countryid) {
+    	$daneKraju = \App\factbook_values::where('countryid', $countryid)->get();
+    	return $daneKraju;
+    }
+
+
+    /**
+    *funkcja zwraca nazwe pola (pole zawiera pojedyncze dane na temat kraju np.: GDP, nazwa kraju ) 
+    * @param int fieldid - id pola
+    * @return - string, nazwa pola
+    */
+    private function zwrocNazwePola($fieldid) {
+    	$nazwaPola = \App\factbook_fields::where('id', $fieldid)->get();
+    	return $nazwaPola[0]->name;
+    }
+
 }
---
+++ app/factbook_fields.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class factbook_fields extends Model
+{
+    //
+}
